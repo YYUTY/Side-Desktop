c674536eb3216adfca217126e6506052b33ef882
YatsuhaAyto <asa2siriaru@gmail.com>
2022-03-07 23:44:16 +0900

    iroiro

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.geometry("640x480")
+    root.iconbitmap(default="./Texture/Icon/icon.ico")
+    root.title("Side")
+    root.mainloop()
+
+main()
