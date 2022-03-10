266e909facedb14dbe5fcd37b18df4d2af7aa1bc
YatsuhaAyto <asa2siriaru@gmail.com>
2022-03-11 03:23:04 +0900

    hiroyuki

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     root.geometry("640x480")
     root.iconbitmap(default="./Texture/Icon/icon.ico")
     root.title("Side")
+    root.configure(background="#fbdac8")
     root.mainloop()
 
 main()
